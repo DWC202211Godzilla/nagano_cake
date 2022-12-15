301089452857cc94268ed3f3b7fb613bfbf619ba
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2022-12-15 08:17:37 +0000

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
