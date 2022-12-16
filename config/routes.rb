e9afefbcb0e617e6f1d441c25b49f70a19b5e5b8
ItohMarina <117239917+itohmarina@users.noreply.github.com>
2022-12-16 07:44:43 +0000

    correct_sign_in_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :controllers
   devise_for :customers, skip:[:passwords],controllers:{
     registrations:"public/registrations",
     sessions: 'public/sessions'
