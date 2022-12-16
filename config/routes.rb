689631155f22b2c72007b7349c1890328bfc6886
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2022-12-16 05:53:22 +0000

    devise_for destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
   namespace :public do
+    get 'registrations/new'
+    get 'registrations/create'
+  end
+  namespace :public do
     get 'items/index'
     get 'items/show'
   end
-  devise_for :admins
-  devise_for :customers
+
 
 
   devise_for :customers, skip:[:passwords],controllers:{
