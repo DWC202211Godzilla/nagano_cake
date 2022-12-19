bf8105769ce25cf680427239d9c66c3f5e061c61
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2022-12-19 04:58:20 +0000

    order create

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,8 +4,13 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    @order = Order.new(order_params)
     @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) {|sum , item| sum += (item.price * 1.1 )}
+
+    @order = Order.new(order_params)
+    @order.shipping_cost = 800
+    @order.total_payment = @order.shipping_cost + @total
+    @order.status = 0
 
     if params[:order][:payment_method] == "credit_card"
       @order.payment_method = 0
@@ -18,7 +23,7 @@ class Public::OrdersController < ApplicationController
       @order.address = current_customer.address
       @order.postal_code = current_customer.post_code
     elsif params[:order][:address_number] == "2"
-      if Address.exists?(name: params[:order][:registered])
+      if Address.all.count != 0
         @order.name = Address.find(params[:order][:registered]).name
         @order.address = Address.find(params[:order][:registered]).address
         @order.postal_code = Address.find(params[:order][:registered]).post_code
@@ -27,6 +32,7 @@ class Public::OrdersController < ApplicationController
       end
     elsif params[:order][:address_number] == "3"
       address_new = current_customer.addresses.new(address_params)
+      address_new.post_code = @order.postal_code
       if address_new.save
       else
         render :new
@@ -40,6 +46,24 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
+    cart_items = current_customer.cart_items.all
+    @order = current_customer.orders.new(order_params)
+
+    if @order.save
+      cart_items.each do |cart|
+        order_detail = OrderDetail.new
+        order_detail.item_id = cart.item_id
+        order_detail.order_id = @order.id
+        order_detail.price = cart.item.price * 1.1
+        order_detail.amount = cart.amount
+        order_detail.save
+      end
+      redirect_to orders_complete_path
+      cart_items.destroy_all
+    else
+      @order = Order.new(order_params)
+      render :new
+    end
   end
 
   def index
@@ -51,10 +75,10 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method,:name,:address,:postal_code)
+    params.require(:order).permit(:payment_method,:name,:address,:postal_code,:shipping_cost,:total_payment,:status)
   end
 
   def address_params
-    params.require(:order).permit(:name,:address,:post_code)
+    params.require(:order).permit(:name,:address)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   has_many :cart_items
+  has_many :order_details
 end
