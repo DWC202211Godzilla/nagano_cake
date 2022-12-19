f392b2316d128ca108940dc358194efd5721cabf
mmiissaakkii <mmmnn1228@gmail.com>
2022-12-19 09:05:13 +0000

    index,new,show

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   has_many :cart_items
+  has_one_attached :image
+  validates :name,presence:true
+  validates :introduction,presence:true,length:{maximum:200}
+  validates :price,numericality: {only_integer: true}
 end
--- db/migrate/20221215054217_create_items.rb
+++ db/migrate/20221215054217_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
       t.integer :price
       t.text :introduction
       t.boolean :is_active
-
+      
       t.timestamps
     end
   end
