6031859831e74865bc2eecfa308412b6ba5ab50a
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2022-12-22 12:25:21 +0000

    [authenticate]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_customer!, except: [:top, :index, :show, :about]
 
   protect_from_forgery with: :exception
   before_action :configre_permitted_parameters, if: :devise_controller?
