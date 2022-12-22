f79a8de93e08ac87b8c2bb7f91f08e82391b82af
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2022-12-22 12:32:17 +0000

    エラーのため

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,45 +1,26 @@
 class Admin::OrdersController < ApplicationController
-  def show
-    @orders = Order.all
-    @order = Order.find(params[:id])
-    @order_details = @order.order_details.all
+ def show
+  @orders = Order.all
+  @order = Order.find(params[:id])
+  @order_details = @order.order_details.all
+ end
+
+ def update
+  @order = Order.find(params[:id])
+  @order.update(order_params)
+
+  if @order.status == "paid_up"
+   @order.order_details.each do |order_detail|
+    order_detail.update(making_status: "production_pending")
+   end
   end
 
-  def update
-    @order = Order.find(params[:id])
-<<<<<<< HEAD
-<<<<<<< HEAD
-    @order_details = @order.order_details.all
-
-    if params[:status]
-      @order.update(order_params)
-      redirect_to admin_order_path(@order)
-    end
-
-    if params[:making_status]
-    end
-
-=======
-=======
->>>>>>> origin/develop
-    @order.update(order_params)
-
-    if @order.status == "paid_up"
-      @order.order_details.each do |order_detail|
-        order_detail.update(making_status: "production_pending")
-      end
-    end
-
-    redirect_to request.referer
-<<<<<<< HEAD
->>>>>>> origin/develop
-=======
->>>>>>> origin/develop
-  end
+  redirect_to request.referer
+ end
 
 
-  private
-  def order_params
-    params.require(:order).permit(:status)
-  end
+ private
+ def order_params
+  params.require(:order).permit(:status)
+ end
 end
