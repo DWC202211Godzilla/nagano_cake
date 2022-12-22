fb41760e503a9c7e2c4f5ad843fbc10737d89074
ItohMarina <117239917+itohmarina@users.noreply.github.com>
2022-12-22 12:38:21 +0000

    authenticate_customer&admin

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,5 @@
 class Admin::OrderDetailsController < ApplicationController
+  before_action :authenticate_admin!
   def update
     @order_details = OrderDetail.find(params[:id])
     @order_details.update(order_detail_params)
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,4 +1,5 @@
 class Public::OrdersController < ApplicationController
+  before_action :authenticate_customer!
   before_action :confirm_match? , only: [:show]
 
   def new
